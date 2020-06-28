f2292f29bbc3bf12ef02a2d173f3b1abb76e5f7d
Devin Dias <devindias.tech@gmail.com>
2020-06-28 17:15:17 -0500

    completes user auth

---
+++ data/migrations/04_tasks.js
@@ -0,0 +1,25 @@
+  
+exports.up = function (knex) {
+    return knex.schema
+  
+      .createTable('tasks', function (table) {
+        table.increments();
+        table
+          .integer('task_id')
+          .references('id')
+          .inTable('task_names')
+          .notNullable();
+        table.integer('user_id').references('id').inTable('users').notNullable();
+        table.string('task_notes');
+        table.integer('category_id').references('id').inTable('categories');
+        table.datetime('due_date').notNullable();
+        table.boolean('all_day');
+        table.boolean('is_complete').defaultTo(false);
+      });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema
+      .dropTable('tasks')
+
+  };
---
+++ server.js
@@ -0,0 +1,25 @@
+
+const express = require("express")
+const configureFunc = require("./middleware/ConfigureAPI")
+// const restricted = require("./middleware/Restrict");
+const userRouter = require("./API/users/routes")
+
+
+
+//Creates Server
+const server = express()
+
+//Configs **Json**Helmet**corse**Morgan***Logger**
+configureFunc(server)
+//Routers
+server.use("/life_logger/auth/users", userRouter)
+
+
+
+//Test route
+server.get("/", (req, res) => {
+    res.status(200).send("<h3>The morning dawn brings thoughts of solidarity. How can they live if we believe in what they have done. The only way for progress is too burn the establishment down \n -Devin Dias </h3>")
+})
+
+
+module.exports = server
